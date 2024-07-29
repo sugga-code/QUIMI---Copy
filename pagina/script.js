9eb19ac2e43182fbe155e0f3dd6fd1b6e19a9f71
sugga-code <agustinnicolini10@gmail.com>
2024-07-29 17:09:30 -0300

    quimi

---
+++ pagina/script.js
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Iniciar Sesión</title>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.3.0/dist/sweetalert2.all.min.js"></script>
+
+    <style>
+
+     a{
+        display: block;
+        width: 110px;
+        font-family: Arial, Helvetica, sans-serif;
+        font-weight: 700;
+        background-color: rgb(127, 206, 255);
+        border-radius: 10px;
+        padding: 15px 30px;
+        margin: 150px 130px;
+        text-decoration-color: wheat;
+        text-decoration: none;
+        transform: translateX(700px) translateY(300px);
+     }
+     a:hover
+     {
+        background-color: transparent;
+        border: 2px solid rgb(127, 206, 255);
+        color: rgb(127, 206, 255);
+     }
+
+
+
+    </style>
+
+
+
+</head>
+
+
+<body>
+        <script src="/js/login.js"></script>
+        <script>
+            menúBásico()
+        </script>
+
+       <a href="bienvenido.html" target="_blank">Entrar</a>
+
+</body>
+</html>
